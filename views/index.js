a0f732cd37032710ec4a78bc94b29695afe0d493
Kanyana <ktoadithep@gmail.com>
2023-02-21 11:27:18 +0700

    .

--- views/index.js
+++ views/index.js
@@ -4,3 +4,53 @@ const sessions = require("express-session");
 
 const app = express();
 const PORT = 3000;
+
+const oneDay = 1000 * 60 * 60 *24;
+app.use(session({
+    secret: "thisismysecrctekey",
+    saveUninitialized:true,
+    cookie: {maxAge: oneDay},
+    resave: false
+}));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.static(__dirname));
+
+app.use(cookieParser());
+
+const myusername = 'admin'
+const mypassword = '1234'
+
+var session;
+
+app.get('/',(req,res) => {
+    session = req.session;
+    if(session.userid){
+        res.send("Walcome User <a hraf=\'/logout'>click to logout</a>");
+    }else
+    res.sendFile('views/index.html',{root:__dirname})
+});
+
+app.post('/user',(req,res) => {
+    if(req.body.username == myusername && req.body.password == mypassword){
+        session=req.session;
+        session.userid=req.body.username;
+        console.log(req.session)
+    }
+    else{
+        res.send('Invalib username or password');
+    }
+})
+
+app.get('/logout',(req,res)=>{
+    req.session.destroy();
+    res.redirect('/');
+});
+
+app.get('*',(req, res) => {
+    res.send('404 not found')
+})
+
+app.listen(PORT, () => consoul.log(`server runing at port ${PORT}`));
