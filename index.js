45cd77a4c6addabd1f25846abba52fd1ea30fd1f
Kanyana <ktoadithep@gmail.com>
2023-03-10 14:53:25 +0700

    .

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const sessions = require("express-session");
@@ -6,7 +7,7 @@ const app = express();
 const PORT = 3000;
 
 const oneDay = 1000 * 60 * 60 *24;
-app.use(session({
+app.use(sessions({
     secret: "thisismysecrctekey",
     saveUninitialized:true,
     cookie: {maxAge: oneDay},
@@ -23,27 +24,27 @@ app.use(cookieParser());
 const myusername = 'admin'
 const mypassword = '1234'
 
-var session;
+var sessions;
 
 app.get('/',(req,res) => {
-    session = req.session;
-    if(session.userid){
+    sessions = req.session;
+    if(sessions.userid){
         res.send("Walcome User <a> hraf=\'/logout'>click to logout</a>");
     }else
-    res.sendFile('views/index.html',{root:__dirname})
+    res.sendFile('views/index.html',{root:__dirname});
 });
 
 app.post('/user',(req,res) => {
     if(req.body.username == myusername && req.body.password == mypassword){
-        session=req.session;
-        session.userid=req.body.username;
-        console.log(req.session)
+        sessions=req.session;
+        sessions.userid=req.body.username;
+        console.log(req.session);
         res.send("hello Walcome<a> hraf=\'/logout'>click to logout</a>");
     }
     else{
         res.send('Invalib username or password');
     }
-})
+});
 
 app.get('/logout',(req,res)=>{
     req.session.destroy();
@@ -51,7 +52,7 @@ app.get('/logout',(req,res)=>{
 });
 
 app.get('*',(req, res) => {
-    res.send('404 not found')
-})
+    res.send('404 not found');
+});
 
 app.listen(PORT, () => consoul.log(`server runing at port ${PORT}`));
